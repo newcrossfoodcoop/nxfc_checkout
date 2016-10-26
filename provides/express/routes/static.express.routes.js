fa94dec1b8b5d341f416bda59b9057d4bc97b097
Ben Simpson <ben@hy-street.net>
2016-10-26 11:51:16 +0100

    whitespace

--- provides/express/routes/static.express.routes.js
+++ provides/express/routes/static.express.routes.js
@@ -4,5 +4,5 @@ var path = require('path');
 var express = require('express');
 
 module.exports = function(app) {
-    app.use('/', express.static(path.resolve(__dirname, '../raml'),{index: 'build/api.html'}));
+    app.use('/', express.static(path.resolve(__dirname, '../raml'), {index: 'build/api.html'}));
 };
